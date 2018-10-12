17c5a2a398fbedad08a3c7f3545df923e499c7ab
franklin de chanaud <f_dechanaud@hotmail.fr>
2018-10-12 17:09:29 +0200

    main methods of crud done

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,7 +1,24 @@
 class ShiftsController < ApplicationController
   def new
+    @shift = Shift.new
   end
 
   def create
+    @shift = Shift.new(shift_params)
+    if @shift.save
+      render :index
+    else
+      render :new
+    end
+  end
+
+  def index
+    @shifts = Shift.all
+  end
+
+  private
+
+  def shift_params
+    pemit(:shift).params(:first_name, :status)
   end
 end
--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -1,32 +1,33 @@
-class WorkersController < ApplicationController
-  before_action :retrieve_worker, only: [:edit, :update]
+class UsersController < ApplicationController
+  before_action :retrieve_user, only: [:edit, :update]
 
   def new
-    @worker = Worker.new
+    @user = User.new
   end
 
   def create
-    @worker = Worker.new(worker_params)
-    if @worker.save
+    @user = User.new(user_params)
+    if @user.save
       redirect_to shifts_path
     else
       render :new
     end
   end
 
-  def edit
-
-  end
+  def edit; end
 
   def update
-
+    @user.update(user_params)
+    redirect_to shifts_path
   end
 
-  def worker_params
-    .pemit(:worker).
+  private
+
+  def user_params
+    pemit(:user).params(:first_name, :status)
   end
 
-  def retrieve_worker
-    @worker ||= Worker.find(params[:id])
+  def retrieve_user
+    @user ||= User.find(params[:id])
   end
 end
